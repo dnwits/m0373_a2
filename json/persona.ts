68cddefeec1cdc60c9ccfcb946737ec754a1fd95
Valery Michelle Escobar <v.m.e.c1122002@gmail.com>
2025-02-18 10:29:57 +0100

    a

--- json/persona.ts
+++ json/persona.ts
@@ -3,7 +3,7 @@ type Persona = {
     edat: number;
 };
 //indicar la ruta relativa como a html
-const persones: Persona[]= await Bun.file("./persona.json").json();
+const persones: Persona[]= await Bun.file("json/persona.json").json();
 persones.forEach((persona, index) =>{
     console.log(`Persona ${index + 1}: Nom: ${persona.nom}, Edat: ${persona.edat}`)
 });
