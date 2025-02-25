dc80dada3d6bbcd58e483dc49d1a3026bbb05df8
Valery Michelle Escobar <v.m.e.c1122002@gmail.com>
2025-02-25 09:40:52 +0100

    falat el de jugadors

--- json/jugadores.tsx
+++ json/jugadores.tsx
@@ -3,9 +3,33 @@ type Jugador = {
     edat: Number;
     equip:string;
 }; 
-const filecontent2  = await Bun.file("jugadorScript.json").text();
-
+// Llegim el fitxer JSON amb informació sobre jugadors
+const filecontent2  = await Bun.file("jugadors.json").text();
+// Convertim el contingut a un array d'objectes Player
 const jugadors: Jugador[] = JSON.parse(filecontent2);
+// Mostrem els jugadors per consola
+jugadors.forEach((player) => {
+    console.log(`Nom: ${player.nom}, Edat: ${player.edat}, Equip: ${player.equip}`);
+});
 
-jugadors.forEach
-//a medias
+const tablaBody1 = document.getElementById("tabla-body");
+
+if (tablaBody1) {
+    jugadors.forEach(jugador => {
+        const fila = document.createElement("tr");
+
+        const celdaNombre = document.createElement("td");
+        celdaNombre.textContent = jugador.nom;
+        fila.appendChild(celdaNombre);
+
+        const celdaEdat = document.createElement("td");
+        celdaEdat.textContent = jugador.edat.toString();
+        fila.appendChild(celdaEdat);
+
+        const celdaEquipo = document.createElement("td");
+        celdaEquipo.textContent = jugador.equip;
+        fila.appendChild(celdaEquipo);
+
+        tablaBody1.appendChild(fila);
+    });
+}
